dd549ddbf34aed4eaae1366d35a2eb0a72b025a4
Feiron <53085628+fr1g@users.noreply.github.com>
2024-06-10 19:21:23 +0800

    stepping

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# on.js
- This is the experimental repo for On.js
+# On.JS (WIP, **NOT USABLE**)
+*This is the experimental repo for On.js*
+
+## What is On.JS?
+
+this is a static-page renderer, which reads jsons to render HTML pages.
+
+## it SHOULD support (todo list)
+
+- [ ] Render Page via JSON
+- [ ] Directly read JSON to build Shadow DOM
+- [ ] internal i18n pre-render
+- [ ] built-in front-end router
+- [ ] dual-binded value-reading from in-json js variable <sup>?</sup>
+- [ ] import components from specified HTML file (binded with file-name and id)
+- [ ] include ordinary HTML file (iframe / render)
+
+## What's shinning?
+
+- Essentials built-in, like i18n(read json list, replace i18n-key with matched stuff, after this render the page.)
+- As easy as we can,  to build a page easily that content-oriented.
+
+## Anything not fit?
+
+you can use this anywhere since this can be mounted anywhere but you should turn internal router before using it in a partial place of your site.
+
+If you use On.JS to build your introducing page or anything else, this is quite fit. Maybe it also can be used to build a  documentation page, but this should not used for a business app that requires high page efficiency and security.
+
--- on.js
+++ on.js
@@ -1,41 +1,111 @@
+/**
+ * TODO:
+ *      json-rendering:
+ *          
+ *      html-as-component-import:
+ *          use id to mark component name
+ *          match via json
+ *          
+ */
+
 class OnInstance{
     Mounted;
     Debug = false;
+    ObjList = [];
     ComponentProcessor = class {
     
     };
+
+    EventCall = {
+        ObjList: {
+            _customCallbacks: [],
+            ObjListChanged: function(describe = ''){
+                console.log(describe);
+                // run callbacks
+            }
+        }
+    }
 
     setDebug(debug = false){
         this.Debug = debug;
         console.log(`On.JS Debug ${this.Debug ? 'ON' : 'OFF'}`);
     }
 
+    addToObjList(source, useExtended = false){
+        let comp = this.ObjList.length, jsonName = '..null', susList = (typeof source != 'string');
+        if(susList){
+            try {
+                for(let item of source){
+                    let subcomp = this.ObjList.length;
+                    this.ObjList.push(On.GetParsed(item, useExtended));
+                    if(!useExtended) jsonName = Util.afn(item);
+                    if(subcomp != this.ObjList.length) this.EventCall.ObjList.ObjListChanged(`ObjList changed: added ${jsonName}`);
+                }
+                
+                jsonName = '..null';
+            } catch (ex) {
+                if(!useExtended) jsonName = Util.afn(source);
+                this.ObjList.push(On.GetParsed(source, useExtended));
+            }
+        }else this.ObjList.push(On.GetParsed(source, useExtended));
+        if(!useExtended) jsonName = Util.afn(source);
+        if(comp != this.ObjList.length) this.EventCall.ObjList.ObjListChanged(`ObjList changed: added ${(jsonName = '..null' && susList ? 'a list' : jsonName)}`);
+    }
+
     constructor(hook){
         this.Mounted = document.getElementById(hook);
         console.log('On.JS Mounted the app-root element');
         
-        this.Mounted.style.backgroundColor = 'aliceblue';
     }
 }
 
 var currentRoute = '/';
 var On = {
-    Route: function(operation = '/'){ 
-        /* default to the root */
-        currentRoute = operation;
-        let pushing = window.history.pushState(history.state || {}, '', operation);
+    Router: {
+        Route: function(operation = '/'){ // wip
+            /* default to the root */
+            currentRoute = operation;
+            let pushing = window.history.pushState(history.state || {}, '', operation);
+        },
     },
-    
-    Get: function(path, useExtended = false){
+    I18n: {
+        RunI18nRender: function(part, LangList){ // wip
+            let rendered;
+            return rendered;
+        }
+    },
+    GetParsed: function(source, useExtended = false){
+        let x;
+        try {
+            x = JSON.parse(On.Get(source, useExtended));
+        } catch (ex) {
+            
+            return null;
+        }
+        return x;
+    },
+    Get: function(source, useExtended = false){
         let returnedJsonRawString = '', fetching;
         if(!useExtended){
-            fetching = fetch(path).then(response => {
-                returnedJsonRawString = response.text();
-                console.log(response);
-                console.log(returnedJsonRawString)
-                return returnedJsonRawString;
-            })
+            fetching = fetch(source).then(response => {
+                if(response.ok){
+                    returnedJsonRawString = response.text();
+                    return returnedJsonRawString;
+                }
+                else return null;
+            }).catch(ex => {
+                console.error(ex);
+                return null;
+            });
         }
         //  else use created respObj
     },
+}
+
+var Util = {
+    afn: function(fn){
+        let analy = fn.split('/');
+        let len = analy.length;
+        return (analy[len - 1]);
+    }
 }
